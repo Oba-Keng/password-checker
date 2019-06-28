1c7b8cd12d42ddd0e82df25e52cbafc015f2803c
Obakeng <obeezus@gmail.com>
2019-06-28 15:20:41 +0200

    second commit

--- spec/password.js
+++ spec/password.js
@@ -1,67 +1,47 @@
-
-function password_is_valid(password){
-
-
-    
+function password_is_valid(password) {
   const upperCase = /[A-Z]/;
   const lowerCase = /[a-z]/;
   const number = /[0-9]/;
 
-//to check if a password strength is competent
-// try {
+  // try {
+  //conditions that check various validation requirements for the password
+  if (password == "") throw new Error("password should not be empty");
 
-    
-   
-
-if (password == "") 
-    throw new Error("password should not be empty");
-
-if (!upperCase.test(password))
+  if (!upperCase.test(password))
     throw new Error("password should contain atleast one Uppercase character");
 
-if (!lowerCase.test(password))
+  if (!lowerCase.test(password))
     throw new Error("password should contain atleast one lowercase character");
 
-if (!number.test(password))
+  if (!number.test(password))
     throw new Error("password should contain atleast one number");
-  
-if (password.length < 8)
+
+  if (password.length < 8)
     throw new Error("password should be more than eight characters");
 
-        
-        
-    
+  // } catch (error) {
+  //     console.log('caught', error)
+  // }
+}
+//checks three different password requirements and returns true if stleast three are met
+function password_is_okay(password) {
+  var passer = new password_is_valid();
 
-    
-
-   
-// } catch (error) {
-//     console.log('caught', error)
-// }
-
-    
-    
-
+  if (
+    password.length < 8 &&
+    !upperCase.test(this.password) &&
+    !number.test(this.password)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function password_is_okay(password){
-
-   var passer = new password_is_valid();
-
-    if(password.length < 8&&!upperCase.test(password)&&!number.test(password)){
-        return true;
-    }else{
-        return false;
-    }
-
-}
-
-module.exports =   {
-    
-    password_is_valid:password_is_valid,
-    password_is_okay:password_is_okay
-
-}
+module.exports = {
+  password_is_valid: password_is_valid,
+  password_is_okay: password_is_okay
+};
 
 // password_is_valid('Todayawayplay1');
 // password_is_okay('Toni6ghTa');
--- spec/passwordChecker.spec.js
+++ spec/passwordChecker.spec.js
@@ -1,41 +1,44 @@
-var password_is_valid = require('./password.js');
+var password_is_valid = require("./password.js");
 var passed = password_is_valid.password_is_valid;
-var password_is_okay = require('./password.js');
+var password_is_okay = require("./password.js");
 var verified = password_is_okay.password_is_okay;
 
-
-
-
 describe("Password Checker", function() {
-  
-it("should check password is not null", function() {
-      expect(function () {
-        passed("")}).toThrow(new Error("password should not be empty"))    
-});
-  
-it("should check password has atleast one Uppercase letter ", function() {
-      expect(function () {
-        passed("r")}).toThrow(new Error("password should contain atleast one Uppercase character"))    
-});
-  
-it("should check password has atleast one lowercase letter", function() {
-      expect(function () {
-        passed("K")}).toThrow(new Error("password should contain atleast one lowercase character"))    
-});
-  
-it("should check password has atleast one number", function() {
-      expect(function () {
-        passed("kS")}).toThrow(new Error("password should contain atleast one number"))
-});
-  
-it("should check password has more than 8 characters", function() {
-      expect(function () {
-        passed("kK1")}).toThrow(new Error("password should be more than eight characters"))
-});  
-  
-it('meets three of password given criteria', function(){
+  it("should check password is not null", function() {
+    expect(function() {
+      passed("");
+    }).toThrow(new Error("password should not be empty"));
+  });
 
-    expect(verified('Typo69gravy').toBe(true));
-})
+  it("should check password has atleast one Uppercase letter ", function() {
+    expect(function() {
+      passed("r");
+    }).toThrow(
+      new Error("password should contain atleast one Uppercase character")
+    );
+  });
 
+  it("should check password has atleast one lowercase letter", function() {
+    expect(function() {
+      passed("K");
+    }).toThrow(
+      new Error("password should contain atleast one lowercase character")
+    );
+  });
+
+  it("should check password has atleast one number", function() {
+    expect(function() {
+      passed("kS");
+    }).toThrow(new Error("password should contain atleast one number"));
+  });
+
+  it("should check password has more than 8 characters", function() {
+    expect(function() {
+      passed("kK1");
+    }).toThrow(new Error("password should be more than eight characters"));
+  });
+
+  it("meets three of password given criteria", function() {
+    expect(verified("tYpo69gravy").toBe(true));
+  });
 });
